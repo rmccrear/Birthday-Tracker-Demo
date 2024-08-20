4849495d9609d7a99815a974e618a66a7e4e8b7d
Robert McCreary Mannino <fooyinfighter@gmail.com>
2024-08-19 18:37:15 -0700

    add form for signup

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
+// this is state
+let state = {
+    name: "",
+    birthday: "",
+    zipCode: 0
+};
+
 console.log("hello world");
+
+let saveBtn = document.getElementById("saveButton");
+let nameInput = document.getElementById("userName");
+let birthdayInput = document.getElementById("birthday");
+let zipCodeInput = document.getElementById("zipcode");
+
+function handleSave() {
+  console.log("save button was clicked");
+  let name = nameInput.value;
+  state.name = name;
+  console.log("Added name" + state.name);
+
+  state.birthday = birthdayInput.value;
+  state.zipCode = zipCodeInput.value;
+
+  console.log(state);
+  // optionally reset this
+  nameInput.value = "";
+  birthdayInput.value = "";
+  zipCodeInput.value = "";
+}
+saveBtn.addEventListener("click", handleSave);
+
+// old way...
+// saveButton.onclick = handleSave;
